2e1aa2ac0bb6d49ccb5f0ac8e7f41aa5c529ce78
Paitel Maximilien <maximilien.paitel@dotsafe.fr>
2016-11-05 14:33:02 +0100

    Added Twitter API PHP Wrapper from J7mbo Added Twitter Histogram endpoint that returns the number of tweets per hour of the day for a given user (limited to a maximum of 200 last tweets) Uptated README.md

--- README.md
+++ README.md
@@ -17,6 +17,19 @@ This application consist of theses endpoints:
 **First thing to do is to install the app depedencies using composer:**
 `composer install`
 
+**Configure Twitter API settings**
+In order to use the twitter histogram call you need to enter valid Twitter API credentials.
+If you don't already have a Twitter account, you can sign up for one at https://twitter.com/.
+You'll need to create a Twitter application for accessing tweets, you can create one at https://dev.twitter.com/apps.
+It only needs to be enabled for read access to Twitter data.
+
+You will need to generate Keys and Access tokens in order to replace the values from those lines in _web/index.php_ file :
+`// Twitter API configuration`
+`$twitter_consumer_key = 'YOUR_TWITTER_CONSUMER_KEY';`
+`$twitter_consumer_secret = 'YOUR_TWITTER_CONSUMER_SECRET';`
+`$twitter_access_token= 'YOUR_TWITTER_ACCESS_TOKEN';`
+`$twitter_access_secret = 'YOUR_TWITTER_ACCESS_TOKEN_SECRET';`
+
 
 The easiest way to test the app without having to do server configuration is to run the PHP built in server on your environment (available since php 5.4)
 
--- web/index.php
+++ web/index.php
@@ -2,6 +2,12 @@
 
 require_once 'vendor/autoload.php';
 
+// Twitter API configuration
+$twitter_consumer_key = 'YOUR_TWITTER_CONSUMER_KEY';
+$twitter_consumer_secret = 'YOUR_TWITTER_CONSUMER_SECRET';
+$twitter_access_token= 'YOUR_TWITTER_ACCESS_TOKEN';
+$twitter_access_secret = 'YOUR_TWITTER_ACCESS_TOKEN_SECRET';
+
 // init Silex app
 $app = new Silex\Application();
 
@@ -15,9 +21,42 @@ $app->get('/hello/{text}', function ($text) {
     return "Hello $text";
 });
 
+
+// Twitter API keys and tokens used
+$twitterSettings = array(
+    'consumer_key' => $twitter_consumer_key,
+    'consumer_secret' => $twitter_consumer_secret,
+    'oauth_access_token' => $twitter_access_token,
+    'oauth_access_token_secret' => $twitter_access_secret
+);
+
 // define route for /histogram/{twitterUsername}
-$app->get('/histogram/{twitterUsername}', function ($twitterUsername) {
-    return "Should return Twitter hour stats for user with screen name $twitterUsername";
+$app->get('/histogram/{twitterUsername}', function ($twitterUsername) use ($twitterSettings) {
+
+    $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+    $getfield = '?screen_name='.$twitterUsername.'&count=200';
+    $requestMethod = 'GET';
+
+    // Perform Twitter API call
+    $twitter = new TwitterAPIExchange($twitterSettings);
+    $tweets =  json_decode($twitter->setGetfield($getfield)
+        ->buildOauth($url, $requestMethod)
+        ->performRequest());
+
+    // Initialize all day hours
+    $response = array();
+    for ($i=0;$i<=24;$i++) {
+        $response[sprintf("%02d", $i).'h'] = 0;
+    }
+
+    // Counts per hour for the user's tweets
+    foreach ($tweets as $tweet) {
+        $tweetDate = DateTime::createFromFormat('D M d H:i:s P Y', (string)$tweet->created_at);
+        $hour = $tweetDate->format('H').'h';
+        $response[$hour] ++;
+    }
+
+    return json_encode($response);
 });
 
 $app->run();
